65ef2fe102f639d22639f51494e3405328b1e93a
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2023-12-14 23:45:41 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import app from "./app.js";
+import { connectionDatabase } from "./config/db.js";
 
 const PORT = process.env.PORT
 
+connectionDatabase();
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
 })
