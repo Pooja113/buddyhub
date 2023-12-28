b89e2bc7e5992e746ed663d688238c080f379ab5
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2023-12-28 22:57:33 +0530

    feat: follow unfollow functionality

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import userController from '../controllers/userController.js';
+import authVerify from '../middlewares/index.js';
 const router = express.Router();
 
 router.post('/user/register', userController.register)
 router.post('/user/login', userController.login)
+router.get('/user/follow/:id', authVerify ,userController.followuser)
 
 
 export default router
